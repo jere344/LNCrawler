f2b4330cf05066c1295d7e7efb3e405ee9caba5d
unknown <jeremy.guerin34@yahoo.com>
2025-06-04 20:22:39 +0200

    optimize sitemap

--- lncrawler-api/lncrawler_api/urls.py
+++ lncrawler-api/lncrawler_api/urls.py
@@ -3,14 +3,14 @@ from rest_framework.routers import DefaultRouter
 from . import views
 from .views import novels_views, comments_views, sources_views, users_views, boards_views, reviews_views
 from .views.csrf import get_csrf_token
-from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps.views import sitemap, index
 from django.views.decorators.cache import cache_page
 from .views.sitemap import (
     StaticViewSitemap,
     NovelSitemap,
     SourceSitemap,
     ChapterListSitemap,
-    ChapterReaderSitemap,
+    # ChapterReaderSitemap,
     ImageGallerySitemap,
 )
 
@@ -23,7 +23,7 @@ sitemaps = {
     'novels': NovelSitemap,
     'sources': SourceSitemap,
     'chapterlists': ChapterListSitemap,
-    'chapters': ChapterReaderSitemap,
+    # 'chapters': ChapterReaderSitemap,
     'galleries': ImageGallerySitemap,
 }
 
@@ -106,6 +106,7 @@ urlpatterns = [
     # CSRF token endpoint
     path('csrf-token/', get_csrf_token, name='csrf_token'),
     
-    # Sitemap
-    path('sitemap.xml', cache_page(3600)(sitemap), {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    # sitemap - 100h cache
+    path('sitemap.xml', cache_page(360000)(index), {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.index'),
+    path('sitemap-<section>.xml', cache_page(360000)(sitemap), {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
--- lncrawler-api/lncrawler_api/views/sitemap.py
+++ lncrawler-api/lncrawler_api/views/sitemap.py
@@ -1,6 +1,7 @@
 from django.contrib.sitemaps import Sitemap
 from django.conf import settings
 from urllib.parse import urlparse
+from django.db import models
 
 from ..models import Novel, NovelFromSource, Chapter
 
@@ -11,6 +12,8 @@ SITEMAP_DOMAIN = parsed_site_url.netloc
 
 
 class BaseSitemap(Sitemap):
+    limit = 1000  # Limit items per sitemap file
+    
     def get_domain(self, site=None):
         return SITEMAP_DOMAIN
 
@@ -20,6 +23,7 @@ class BaseSitemap(Sitemap):
 class StaticViewSitemap(BaseSitemap):
     priority = 0.8
     changefreq = 'weekly'
+    limit = 50  # Static pages don't need pagination
 
     def items(self):
         # These correspond to paths in your App.tsx
@@ -27,9 +31,9 @@ class StaticViewSitemap(BaseSitemap):
             '/',
             '/login',
             '/register',
-            '/profile', # Generic link
-            '/library', # Generic link
-            '/history', # Generic link
+            '/profile',
+            '/library',
+            '/history',
             '/download',
             '/novels/search',
         ]
@@ -42,7 +46,7 @@ class NovelSitemap(BaseSitemap):
     changefreq = 'daily'
 
     def items(self):
-        return Novel.objects.all()
+        return Novel.objects.only('slug', 'updated_at').order_by('slug')
 
     def lastmod(self, obj):
         return obj.updated_at
@@ -55,7 +59,9 @@ class SourceSitemap(BaseSitemap):
     changefreq = 'daily'
 
     def items(self):
-        return NovelFromSource.objects.select_related('novel').all()
+        return NovelFromSource.objects.select_related('novel').only(
+            'source_slug', 'updated_at', 'novel__slug'
+        ).order_by('novel__slug', 'source_slug')
 
     def lastmod(self, obj):
         return obj.updated_at
@@ -68,7 +74,9 @@ class ChapterListSitemap(BaseSitemap):
     changefreq = 'daily'
 
     def items(self):
-        return NovelFromSource.objects.select_related('novel').filter(chapters__isnull=False).distinct()
+        return NovelFromSource.objects.select_related('novel').only(
+            'source_slug', 'updated_at', 'last_chapter_update', 'novel__slug'
+        ).filter(chapters__isnull=False).distinct().order_by('novel__slug', 'source_slug')
 
     def lastmod(self, obj):
         return obj.last_chapter_update or obj.updated_at
@@ -76,47 +84,47 @@ class ChapterListSitemap(BaseSitemap):
     def location(self, obj):
         return f'/novels/{obj.novel.slug}/{obj.source_slug}/chapterlist/'
 
-class ChapterReaderSitemap(BaseSitemap):
-    priority = 0.6
-    changefreq = 'weekly'
+# class ChapterReaderSitemap(BaseSitemap):
+#     priority = 0.6
+#     changefreq = 'weekly'
 
-    def items(self):
-        chapters_to_include = []
-        novel_sources = NovelFromSource.objects.select_related('novel').all()
+#     def items(self):
+#         chapters_to_include = []
+#         novel_sources = NovelFromSource.objects.select_related('novel').all()
 
-        for nfs in novel_sources:
-            # Get the first chapter with content
-            first_chapter = Chapter.objects.filter(
-                novel_from_source=nfs,
-                has_content=True
-            ).order_by('chapter_id').first()
+#         for nfs in novel_sources:
+#             # Get the first chapter with content
+#             first_chapter = Chapter.objects.filter(
+#                 novel_from_source=nfs,
+#                 has_content=True
+#             ).order_by('chapter_id').first()
 
-            if first_chapter:
-                chapters_to_include.append(first_chapter)
+#             if first_chapter:
+#                 chapters_to_include.append(first_chapter)
 
-            # Get the last chapter with content
-            last_chapter = Chapter.objects.filter(
-                novel_from_source=nfs,
-                has_content=True
-            ).order_by('-chapter_id').first()
+#             # Get the last chapter with content
+#             last_chapter = Chapter.objects.filter(
+#                 novel_from_source=nfs,
+#                 has_content=True
+#             ).order_by('-chapter_id').first()
 
-            if last_chapter:
-                # Avoid adding the same chapter twice if it's the only chapter
-                if not first_chapter or first_chapter.id != last_chapter.id:
-                    chapters_to_include.append(last_chapter)
+#             if last_chapter:
+#                 # Avoid adding the same chapter twice if it's the only chapter
+#                 if not first_chapter or first_chapter.id != last_chapter.id:
+#                     chapters_to_include.append(last_chapter)
         
-        return chapters_to_include
+#         return chapters_to_include
 
-    def lastmod(self, obj):
-        # obj is a Chapter instance.
-        # Use last_chapter_update from NovelFromSource if available, else NovelFromSource.updated_at.
-        # Chapter model itself does not have an updated_at field per user's file.
-        if obj.novel_from_source:
-            return obj.novel_from_source.last_chapter_update or obj.novel_from_source.updated_at
-        return None # Should ideally not happen if data is consistent
+#     def lastmod(self, obj):
+#         # obj is a Chapter instance.
+#         # Use last_chapter_update from NovelFromSource if available, else NovelFromSource.updated_at.
+#         # Chapter model itself does not have an updated_at field per user's file.
+#         if obj.novel_from_source:
+#             return obj.novel_from_source.last_chapter_update or obj.novel_from_source.updated_at
+#         return None # Should ideally not happen if data is consistent
 
-    def location(self, obj):
-        return f'/novels/{obj.novel_from_source.novel.slug}/{obj.novel_from_source.source_slug}/chapter/{obj.chapter_id}/'
+#     def location(self, obj):
+#         return f'/novels/{obj.novel_from_source.novel.slug}/{obj.novel_from_source.source_slug}/chapter/{obj.chapter_id}/'
 
 class ImageGallerySitemap(BaseSitemap):
     priority = 0.5
@@ -125,7 +133,9 @@ class ImageGallerySitemap(BaseSitemap):
     def items(self):
         # Consider only sources that actually have images (e.g., cover or chapter images)
         # For simplicity, linking all sources; frontend can handle empty galleries.
-        return NovelFromSource.objects.select_related('novel').all()
+        return NovelFromSource.objects.select_related('novel').only(
+            'source_slug', 'updated_at', 'novel__slug'
+        ).order_by('novel__slug', 'source_slug')
 
     def lastmod(self, obj):
         return obj.updated_at
